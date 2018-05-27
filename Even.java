2d06b9d39dc6ad0acb53f54501050e27ee507c43
evrishi9239 <37260590+evrishi9239@users.noreply.github.com>
2018-05-28 00:54:11 +0530

    commit
    commit

--- Even.java
+++ Even.java
@@ -3,21 +3,9 @@ public class Even
 {
 public static void main(String args[])
 {
-Scanner sc=new Scanner(System.in);
-int a[]=new int[10];
-for(int i=0;i<=9;i++)
-{
-System.out.println("Ten numbers are");
-a[i]=sc.nextInt();
-if(a[i]%2==0)
-{
-System.out.println("True");
+System.out.println("Success");
 }
-else
-{
-System.out.println("False");
 }
-}}}
 
 
 
